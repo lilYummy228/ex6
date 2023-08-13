e893c18123408ae039d531f3f9c56b169804429e
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-08-13 19:19:34 +0500

    добавление переменных

--- ex6/Program.cs
+++ ex6/Program.cs
@@ -10,6 +10,17 @@ namespace ex6
     {
         static void Main(string[] args)
         {
+            int receptionTimeInMinutes = 10;
+            int minutesInHour = 60;
+            Console.Write("Кол-во людей в очереди: ");
+            int peopleCount = Convert.ToInt32(Console.ReadLine());
+
+            int totalMinutes = receptionTimeInMinutes * peopleCount;
+            int timeInHours = totalMinutes / minutesInHour;
+            int timeInMinutes = totalMinutes % minutesInHour;
+
+            Console.WriteLine($"Вы должны простоять в очереди {timeInHours} часа {timeInMinutes} минут");
+
         }
     }
 }
